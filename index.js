e84214147a1315e9b01aa5a44868984e787836b4
chukwuemekaajah <talk2ajah@gmail.com>
2017-10-17 09:28:00 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+
+var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/couponValidator', function (req, res) {
+  var data = {
+    "bestAnimals": [
+      "wombat",
+      "corgi",
+      "puffer fish",
+      "owl",
+      "crow"
+    ]
+  };
+
+  res.json(data);
+});
+
+app.get('/',function(req,res){
+  res.send('hello world')
+})
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
